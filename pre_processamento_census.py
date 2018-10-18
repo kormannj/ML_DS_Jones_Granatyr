8d53fa7fdd44cab3f215f2126324cfe7634aed9a
kormannj <33202837+kormannj@users.noreply.github.com>
2018-10-18 15:28:44 -0300

    Add files via upload

--- pre_processamento_census.py
+++ pre_processamento_census.py
@@ -4,7 +4,7 @@ base = pd.read_csv('census.csv')
 previsores = base.iloc[:,0:14].values
 classe = base.iloc[:,14].values
 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 labelencoder_previsores = LabelEncoder()
 previsores[:,1] = labelencoder_previsores.fit_transform(previsores[:,1])
@@ -15,3 +15,12 @@ previsores[:,7] = labelencoder_previsores.fit_transform(previsores[:,7])
 previsores[:,8] = labelencoder_previsores.fit_transform(previsores[:,8])
 previsores[:,9] = labelencoder_previsores.fit_transform(previsores[:,9])
 previsores[:,13] = labelencoder_previsores.fit_transform(previsores[:,13])
+
+#A OneHotEncoder é a biblioteca usada para transformar variáveis nominais em variáveis dummy
+onehotencoder = OneHotEncoder(categorical_features=[1,3,5,6,7,8,9,13])
+previsores = onehotencoder.fit_transform(previsores).toarray()
+
+#A classe não faz sentido ser transformada para uma variável Dummy, por isso utilizaremos a LabelEncoder e
+#não o OneHotEncoder
+labelencoder_classe = LabelEncoder()
+classe = labelencoder_classe.fit_transform(classe)
